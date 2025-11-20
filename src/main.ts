162dc007c1a5d4e635a9062c5a710755c549abc6
Ted S <tedshi3@gmail.com>
2025-11-19 18:30:47 -0800

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The API documentation for your NestJS application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // -> /api
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(
+    `Swagger running at http://localhost:${process.env.PORT ?? 3001}/api`,
+  );
 }
 bootstrap();
